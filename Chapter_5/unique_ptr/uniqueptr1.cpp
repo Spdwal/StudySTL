6c32f7d6eb9608783fb1f9785d0ee6b13d0e3d65
spdwal <568741362@qq.com>
2017-10-26 22:05:03 +0800

    uniqueptr

---
+++ Chapter_5/unique_ptr/uniqueptr1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+#include<memory>
+#include<dirent.h>
+#include<cstring>
+#include<cerrno>
+
+using namespace std;
+
+class DirCloser{
+public:
+    void operator()(DIR* dp){
+        if(closedir(dp) != 0)
+        {
+            std::cerr << "OOPS: closerdir() failed" << std::endl;
+        }
+    }
+};
+
+int main()
+{
+    unique_ptr<DIR, DirCloser> pDir(opendir("."));
+
+    struct dirent *dp;
+
+    while((dp = readdir(pDir.get())) != nullptr){
+        string filename(dp->d_name);
+        cout << "process " << filename << endl;
+    }
+    return 0;
+}
