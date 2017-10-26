531cd0132e0a7b8b200e2fa44f8bfbbf541872e7
spdwal <568741362@qq.com>
2017-10-26 20:46:02 +0800

    shared_ptr

---
+++ Chapter_5/share_ptr/shareptr1.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<memory>
+
+using namespace std;
+
+int main()
+{
+    shared_ptr<string> pNico(new string("nico"));
+    shared_ptr<string> pJutta(new string("jutta"));
+
+    (*pNico)[0] = 'N';
+    pJutta->replace(0,1,"J");
+
+    vector<shared_ptr<string>> whoMadeCoffee;
+    whoMadeCoffee.push_back(pJutta);
+    whoMadeCoffee.push_back(pJutta);
+    whoMadeCoffee.push_back(pNico);
+    whoMadeCoffee.push_back(pJutta);
+    whoMadeCoffee.push_back(pNico);
+    for(auto ptr : whoMadeCoffee)
+    {
+        cout << *ptr << endl;
+    }
+
+
+    *pNico = "Nicolai";
+
+    for(auto ptr : whoMadeCoffee)
+    {
+        cout << *ptr << endl;
+    }
+
+    cout <<"user_count:" << whoMadeCoffee[0].use_count() << endl;
+
+    return 0;
+}
---
+++ Chapter_5/share_ptr/weakptr3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<memory>
+#include<string>
+#include<exception>
+using namespace std;
+
+int main()
+{
+    try{
+        shared_ptr<string> sp(new string("hi"));
+        weak_ptr<string> wp = sp;
+        sp.reset();
+        cout << wp.use_count() << endl;
+        cout << boolalpha << wp.expired() << endl;
+        shared_ptr<string> p(wp);
+    }catch(const std::exception & e)
+    {
+        cerr << "exception: " << e.what() << endl;
+    }
+    return 0;
+}
