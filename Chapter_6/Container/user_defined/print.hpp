2c565d5ac24bbcefcbe08dba83a33b6dc58e90ea
spdwal <568741362@qq.com>
2017-10-30 10:24:02 +0800

    Manipulating Algorithm

---
+++ Chapter_6/Container/user_defined/print.hpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+
+template<typename T>
+inline void PRINT_ELEMENTS(const T& coll,
+                           const std::string& optstr="")
+{
+    std::cout << optstr;
+    for(const auto& elem : coll)
+    {
+        std::cout << elem << ' ';
+    }
+
+    std::cout << std::endl;
+    //fuck 毫无意义
+}
