d012028fb0fa4122c123679233a3df81aed70350
spdwal <568741362@qq.com>
2017-10-30 11:21:29 +0800

    functionArg

---
+++ Chapter_6/Container/functionArg/sort1.cpp
@@ -0,0 +1,26 @@
+#include<algorithm>
+#include<deque>
+#include<string>
+#include<iostream>
+
+using namespace std;
+
+class Person{
+public:
+    string firstname() const;
+    string secondname() const;
+};
+
+
+boll personSortCriterion(const Person &p1, const Person& p2)
+{
+    return p1.lastname() < p2.lastname() || (p1.lastname()==p2.lastname() && p1.firstname() < p2.lastname());
+}
+
+int main()
+{
+    deque<Person> coll;
+    sort(coll.begin(), coll.end(), personSortCriteriononst);
+
+    return 0;
+}
