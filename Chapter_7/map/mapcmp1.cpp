c9979ca754b4513f8d336c6168ac86f67aeb4ac2
spdwal <568741362@qq.com>
2017-11-01 22:19:14 +0800

    map

---
+++ Chapter_7/map/mapcmp1.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+#include<iomanip>
+#include<map>
+#include<string>
+#include<algorithm>
+#include<cctype>
+
+using namespace std;
+
+
+
+class RuntimeStringCmp{
+public:
+
+	enum cmp_mode {normal, nocase};
+private:
+
+	const cmp_mode mode;
+
+
+	static bool nocase_compare(char c1, char c2){
+		return toupper(c1) < toupper(c2);
+	}
+public:
+
+	RuntimeStringCmp(cmp_mode m=normal) : mode(m){
+	}
+
+
+
+	bool operator() (const string& s1, const string &s2) const {
+        if(mode == normal){
+            return s1 < s2;
+        }else{
+            return lexicographical_compare(s1.begin(),s1.end(),
+                                           s2.begin(),s2.end(),
+                                           nocase_compare);
+        }
+    }
+};
+
+
+
+
+
+typedef map<string,string, RuntimeStringCmp> StringStringMap;
+
+
+void fillAndPrint(StringStringMap& coll);
+
+int main()
+{
+
+    StringStringMap coll1;
+    fillAndPrint(coll1);
+
+
+    RuntimeStringCmp ingorecase(RuntimeStringCmp::nocase);
+
+
+    StringStringMap coll2(ingorecase);
+    fillAndPrint(coll2);
+}
+
+void fillAndPrint(StringStringMap& coll)
+{
+
+    coll["Deutschland"] = "Germany";
+    coll["deutsch"] = "Germany";
+    coll["Haken"] = "snag";
+    coll["arbeiten"] = "work";
+    coll["Hund"] = "dog";
+    coll["gehen"] = "go";
+    coll["Unternehmen"] = "enterprise";
+    coll["unternehmen"] = "undertake";
+    coll["gehen"] = "walk";
+    coll["Bestatter"] = "undertaker";
+
+
+    cout.setf(ios::left, ios::adjustfield);
+	for(const auto& elem: coll) {
+        cout << setw(15) << elem.first << " "
+             << elem.second << endl;
+    }
+
+    cout << endl;
+}
