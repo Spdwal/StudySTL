f559e0cba4a1f991ba805f320fd3156096fbe5f0
spdwal <568741362@qq.com>
2017-11-06 20:59:22 +0800

    modifying algorithm

---
+++ Chapter_11/modifying/unique3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm>
+#include<iterator>
+using namespace std;
+
+bool bothSpaces(char elem1, char elem2)
+{
+	return elem1 == ' ' && elem2 == ' ';
+}
+
+int main()
+{
+	cin.unsetf(ios::skipws);
+	unique_copy(istream_iterator<char>(cin),
+				istream_iterator<char>(),
+				ostream_iterator<char>(cout),
+				bothSpaces);
+	return 0;
+}
