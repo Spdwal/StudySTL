25f0c94d3524da3f1fa1dd6daae66fe9bf0949f6
spdwal <568741362@qq.com>
2017-11-08 19:30:55 +0800

    sorted range algorithm

---
+++ Chapter_11/sortedRange/includes1.cpp
@@ -0,0 +1,26 @@
+#include"algostuff.hpp"
+using namespace std;
+
+int main()
+{
+	list<int> coll;
+	vector<int> search;
+
+	INSERT_ELEMENTS(coll, 1, 9);
+	PRINT_ELEMENTS(coll, "coll:   ");
+
+	search.push_back(3);
+	search.push_back(4);
+	search.push_back(7);
+
+	PRINT_ELEMENTS(search, "search:  ");
+
+	if(includes(coll.cbegin(), coll.cend(),
+				search.cbegin(), search.cend()))
+	{
+		cout << "all elements of search are also in coll"
+			 << endl;
+	}
+
+	return 0;
+}
