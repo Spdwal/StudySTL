32015c570eaf257d01a32d3476a0008a86f94ec1
spdwal <568741362@qq.com>
2018-01-15 09:40:42 +0800

    issue #3

--- Chapter_19/maalloc11.hpp
+++ Chapter_19/maalloc11.hpp
@@ -1,37 +1,45 @@
 #include<cstddef>
 
+
 template<typename T>
 class MyAlloc{
 public:
-	typedef T value_type;
+    typedef T value_type;
 
-	myAlloc() noexcept{
+    myAlloc() noexcept{
 		
-	}
-	template<typename U>
-	myAlloc(const MyAlloc<U>&) noexcept{
+    }
+    template<typename U>
+    myAlloc(const MyAlloc<U>&) noexcept{
 		
-	}
+    }
 
-	T* allocate(std::size_t num){
-		return static<T*>(::operator new(num*sizeof(T)));
-	}
+    T* allocate(std::size_t num){
+        return static_cast<T*>(::operator new(num*sizeof(T)));
+    }
 
-	void deallocate(T* p, sid::size_t num){
-		::operator delete(p);
-	}
+    void deallocate(T* p, std::size_t num){
+        ::operator delete(p);
+    }
 };
 
-template<typename T1, typename T2>
-bool operator==(const MyAlloc<T1>&,
-				const MyAlloc<T2>&) noexcept
+templete<typename T>
+bool operator==(const MyAlloc<T>&a
+                const MyAlloc<T>&b) noexcept
 {
-	return true;
+    return true;
 }
 
 template<typename T1, typename T2>
-bool operator!=(const MyAlloc<T1>&,
-				const MyAlloc<T2>&) noexcept
+bool operator==(const MyAlloc<T1>&a,
+                const MyAlloc<T2>&b) noexcept
 {
-	return false;
+    return false;
+}
+
+template<typename T1, typename T2>
+bool operator!=(const MyAlloc<T1>&a,
+                const MyAlloc<T2>&b) noexcept
+{
+    return !(a==b);
 }
